f9596f86b9f533ad1809be4067ee922161c06359
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-09-20 22:16:30 +0300

    [CHORE]: add error handler

--- app.js
+++ app.js
@@ -2,16 +2,20 @@
 
 const utils = {
     getCanvas(id) {
-        return document.querySelector(".webgl");
+        const canvas = document.getElementById(id);
+        if (!canvas) {
+            console.error(`There is no canvas with id ${id} on this page.`);
+            return null;
+        }
+        return canvas;
     },
 
     getGLContex(canvas) {
-        return canvas.getContext("webgl2");
+        return canvas.getContext("webgl2") || console.error('WebGL2 is not available in your browser.');
     }
 };
 
 (function init() {
     const canvas = utils.getCanvas("webgl");
     gl = utils.getGLContex(canvas);
-    console.log(gl)
 })()
