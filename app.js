362e449dce29d352d29df6187ac3d2dcddd2a84c
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-09-21 12:51:59 +0300

    create a 3D sphere

--- app.js
+++ app.js
@@ -18,4 +18,75 @@ const utils = {
 (function init() {
     const canvas = utils.getCanvas("webgl");
     gl = utils.getGLContex(canvas);
+    // Définir la couleur d'effacement comme étant le noir, complètement opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Effacer le tampon de couleur avec la couleur d'effacement spécifiée
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Définir les vertices d'une sphère
+    const positions = [];
+    const numSegments = 64; // Le nombre de segments pour la sphère
+
+    for (let lat = 0; lat <= numSegments; lat++) {
+        const theta = (lat * Math.PI) / numSegments;
+        const sinTheta = Math.sin(theta);
+        const cosTheta = Math.cos(theta);
+
+        for (let long = 0; long <= numSegments; long++) {
+            const phi = (long * 2 * Math.PI) / numSegments;
+            const sinPhi = Math.sin(phi);
+            const cosPhi = Math.cos(phi);
+
+            const x = cosPhi * sinTheta;
+            const y = cosTheta;
+            const z = sinPhi * sinTheta;
+
+            positions.push(x, y, z);
+        }
+    }
+
+    // Créer un tampon pour les positions
+    const positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    // Créer un programme WebGL
+    const vertexShaderSource = `
+        // Vertex Shader
+        attribute vec3 aPosition;
+        void main(void) {
+            gl_Position = vec4(aPosition, 1.0);
+        }
+    `;
+
+    const fragmentShaderSource = `
+        // Fragment Shader
+        void main(void) {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Rouge
+        }
+    `;
+
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderSource);
+    gl.compileShader(vertexShader);
+
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderSource);
+    gl.compileShader(fragmentShader);
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    // Lier le tampon de position au programme
+    const positionAttributeLocation = gl.getAttribLocation(shaderProgram, 'aPosition');
+    gl.enableVertexAttribArray(positionAttributeLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.vertexAttribPointer(positionAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+
+    // Dessiner la sphère
+    const numVertices = positions.length / 3;
+    gl.drawArrays(gl.POINTS, 0, numVertices);
 })()
